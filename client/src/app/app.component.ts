684210dd1bb94e23ba81b5d73428852cdfcd2fb3
Michał Majchrzak <mmajchrzak1@sii.pl>
2023-09-22 08:24:27 +0200

    ping server added

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) {}
+
   title = 'client';
 
   onActivate(event: any) {
@@ -13,6 +15,18 @@ export class AppComponent {
       top: 0,
       left: 0,
       behavior: 'smooth',
+    });
+  }
+
+  // check if server is online
+  baseURL = 'https://foodappserver-lvot.onrender.com';
+  pingServer() {
+    return this.http.get(`${this.baseURL}`);
+  }
+
+  ngOnInit(): void {
+    this.pingServer().subscribe((res) => {
+      console.log(res);
     });
   }
 }
