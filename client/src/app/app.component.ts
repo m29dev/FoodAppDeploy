432717be7beeb02a3d0cab7ac49546007643640e
Michał Majchrzak <mmajchrzak1@sii.pl>
2023-09-22 09:11:58 +0200

    rem ping server

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   constructor(private http: HttpClient) {}
 
   title = 'client';
@@ -20,24 +20,29 @@ export class AppComponent implements OnInit {
   }
 
   // check if server is online
-  baseURL = 'https://foodappserver-lvot.onrender.com';
-  pingServer() {
-    return this.http.get(`${this.baseURL}`);
-  }
+  // baseURL = 'https://foodappserver-lvot.onrender.com';
+  // pingServer() {
+  //   console.log('PING SERVER');
+  //   return this.http.get(`${this.baseURL}`).subscribe({
+  //     next: (res) => {
+  //       console.log('new res: ', res);
+  //       this.isServerOnline = true;
+  //     },
+  //     error: (err) => {
+  //       console.log('new error: ', err);
+  //       if (err) {
+  //         console.log('display text to wait');
+  //         this.isServerOnline = false;
 
-  ngOnInit(): void {
-    this.pingServer().subscribe({
-      next: (res) => {
-        console.log('new res: ', res);
-        this.isServerOnline = true;
-      },
-      error: (err) => {
-        console.log('new error: ', err);
-        if (err) {
-          console.log('display text to wait');
-          this.isServerOnline = false;
-        }
-      },
-    });
-  }
+  //         setTimeout(() => {
+  //           this.pingServer();
+  //         }, 10000);
+  //       }
+  //     },
+  //   });
+  // }
+
+  // ngOnInit(): void {
+  //   this.pingServer();
+  // }
 }
