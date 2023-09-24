c8795e3f3209e07f0afb50ae8f8dbdaa49fad530
Michał Majchrzak <mmajchrzak1@sii.pl>
2023-09-24 19:10:35 +0200

    orders controller update

--- server/controllers/ordersController.js
+++ server/controllers/ordersController.js
@@ -1,38 +1,9 @@
 const Order = require('../models/Order')
 
-const get_orders = async (req, res) => {
-    try {
-        const today = new Date()
-        let tDay = today.getDate()
-        let tMonth = today.getMonth() + 1
-        let tYear = today.getFullYear()
-        const tDate = `${tDay}/${tMonth}/${tYear}`
-
-        const orders = await Order.find()
-
-        let todaysOrders = []
-
-        orders.forEach((item) => {
-            let day = item.createdAt.getDate()
-            let month = item.createdAt.getMonth() + 1
-            let year = item.createdAt.getFullYear()
-            const date = `${day}/${month}/${year}`
-
-            if (date === tDate) {
-                todaysOrders.push(item)
-            }
-        })
-
-        res.status(200).json(todaysOrders)
-    } catch (err) {
-        console.log(err)
-    }
-}
-
 const get_user_order = async (req, res) => {
     try {
         const email = req.params.email
-        const userOrders = await Order.find({ email }).sort({ date: -1 })
+        const userOrders = await Order.find({ email }).sort({ createdAt: -1 })
         res.status(200).json(userOrders)
     } catch (err) {
         console.log(err)
@@ -40,6 +11,5 @@ const get_user_order = async (req, res) => {
 }
 
 module.exports = {
-    get_orders,
     get_user_order,
 }
--- server/routes/ordersRoutes.js
+++ server/routes/ordersRoutes.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/ordersController')
 
-router.get('/orders', controller.get_orders)
 router.get('/orders/users/:email', controller.get_user_order)
 
 module.exports = router
